c82aeb2a066ab11aefdef3170a755df13e95bc41
soufiane chaoufi <soufiane.chaoufi@gmail.com>
2023-11-07 21:32:38 -0800

    Add SearchBox component

--- app/components/index.ts
+++ app/components/index.ts
@@ -2,3 +2,4 @@ export { Navbar } from "./Navbar";
 export { ThemeProviders } from "./Providers";
 export { SubNavbar } from "./SubNavbar";
 export { Results } from "./Results";
+export { SearchBox } from "./SearchBox";

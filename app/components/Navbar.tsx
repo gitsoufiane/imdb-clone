1514d2a406f19bc61b9398e71358946042e48947
soufiane chaoufi <soufiane.chaoufi@gmail.com>
2023-11-08 10:59:27 -0800

    Update Navbar component styling and links

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,18 +5,21 @@ import { DarkModeSwitcher } from "./DarkModeSwitcher";
 
 export function Navbar() {
   return (
-    <nav className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6">
-      <div className="flex ">
-        <MenuItem title="Home" address="/" Icon={FaHouse} />
-        <MenuItem title="About" address="/about" Icon={FaPerson} />
+    <nav className="flex justify-between mx-8 max-w-6xl sm:mx-auto items-center py-6">
+      <div className="flex space-x-4 lg:space-x-6">
+        <Link href={"/"} className="  hover:text-amber-600">
+          <FaHouse className="text-2xl sm:hidden mx-4" />
+          <p className="hidden sm:inline my-2 text-sm">Home</p>
+        </Link>
+        <Link href={"/about"} className=" hover:text-amber-600">
+          <FaPerson className="text-2xl sm:hidden mx-4" />
+          <p className="hidden sm:inline my-2 text-sm">About</p>
+        </Link>
       </div>
       <div className="flex items-center space-x-5">
         <DarkModeSwitcher />
-        <Link href="/">
-          <h2 className="text-2xl">
-            <span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1">IMDb</span>
-            <span className="text-xl hidden sm:inline">Clone</span>
-          </h2>
+        <Link href="/" className="text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg">
+          IMDb
         </Link>
       </div>
     </nav>

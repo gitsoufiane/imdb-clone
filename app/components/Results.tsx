1a8a6829b97f496ed1c62fb9c70ec46bcdd559ab
soufiane chaoufi <soufiane.chaoufi@gmail.com>
2023-11-07 20:21:29 -0800

    Add Results component for displaying search results

---
+++ app/components/Results.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+import Image from "next/image";
+import { FiThumbsUp } from "react-icons/fi";
+
+export function Results({ results }: { results: any[] }) {
+  return (
+    <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4">
+      {results.map((result) => {
+        return (
+          <div
+            key={result.id}
+            className="cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200 group"
+          >
+            <Link href={`/movie/${result.id}`}>
+              <Image
+                src={`https://image.tmdb.org/t/p/original${
+                  result.backdrop_path || result.poster_path
+                }`}
+                width={500}
+                height={300}
+                className="sm:rounded-t-lg group-hover:opacity-80 transition-opacity duration-200"
+                style={{
+                  maxWidth: "100%",
+                  height: "auto",
+                }}
+                placeholder="blur"
+                blurDataURL="/spinner.svg"
+                alt="image is not available"
+              ></Image>
+              <div className="p-2">
+                <p className="line-clamp-2 text-md">{result.overview}</p>
+                <h2 className="truncate text-lg font-bold">{result.title || result.name}</h2>
+                <p className="flex items-center">
+                  {result.release_date || result.first_air_date}
+                  <FiThumbsUp className="h-5 mr-1 ml-3" /> {result.vote_count}
+                </p>
+              </div>
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
--- app/components/index.ts
+++ app/components/index.ts
@@ -1,3 +1,4 @@
 export { Navbar } from "./Navbar";
 export { ThemeProviders } from "./Providers";
 export { SubNavbar } from "./SubNavbar";
+export { Results } from "./Results";

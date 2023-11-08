e2059f049303f708ffb309c43f3192fbb9cc7574
soufiane chaoufi <soufiane.chaoufi@gmail.com>
2023-11-07 20:21:14 -0800

    Add dynamic movie search functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,27 @@
-export default function Home() {
-  return <main className="">Home</main>;
+import { Results } from "@/app/components";
+
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({ searchParams }: { searchParams: any }) {
+  const genre = searchParams.genre || "fetchTrending";
+
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data"); //this will be caught by the error page and passed to the page as props
+  }
+
+  const data = await res.json();
+  const results = data.results;
+
+  return (
+    <main className="">
+      <Results results={results} />
+    </main>
+  );
 }
